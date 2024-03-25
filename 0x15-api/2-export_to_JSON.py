eedc9a55a2dff0110b695e43b9e815929b4aff56
Alex Kasema <lexxmulwa@gmail.com>
2024-03-25 06:09:51 -0700

    Export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+""" Export to JSON """
+
+from requests import get
+import json
+import sys
+
+
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    res = get(url)
+    username = res.json().get('username')
+
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
+    res = get(url)
+    tasks = res.json()
+
+    data = {user_id: []}
+
+    for task in tasks:
+        data[user_id].append({
+                              "task": task.get('title'),
+                              "completed": task.get('completed'),
+                              "username": username
+                              })
+
+    with open('{}.json'.format(user_id), 'w') as file:
+        json.dump(data, file)
--- 0x15-api/README.md
+++ 0x15-api/README.md
@@ -12,3 +12,10 @@ Requirements:
 Records all tasks that are owned by this employee
 Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
 File name must be: USER_ID.csv
+## 2-export_to_JSON.py
+Export to JSON
+Requirements:
+
+* Records all tasks that are owned by this employee
+* Format must be: { "USER_ID": [{"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, {"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, ... ]}
+* File name must be: USER_ID.json
