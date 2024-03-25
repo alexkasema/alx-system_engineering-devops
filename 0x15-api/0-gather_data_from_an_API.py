55b47d496a1b9d2654d1440ca730faaeaeed54d0
Alex Kasema <lexxmulwa@gmail.com>
2024-03-25 05:29:57 -0700

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""" Gather data from an API """
+
+import sys
+from requests import get
+
+
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    res = get(url)
+    name = res.json().get('name')
+
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
+    res = get(url)
+    tasks = res.json()
+
+    completed = 0
+    completed_tasks = []
+
+    for task in tasks:
+        if task.get('completed'):
+            completed_tasks.append(task)
+            completed += 1
+
+    print('Employee {} is done with tasks({}/{})'
+          .format(name, completed, len(tasks)))
+
+    for task in completed_tasks:
+        print('\t {}'.format(task.get('title')))
---
+++ 0x15-api/README.md
@@ -0,0 +1,7 @@
+# API
+An application program interface (API) is a set of routines, protocols, and tools for building software applications. Basically, an API specifies how software components should interact. Additionally, APIs are used when programming graphical user interface (GUI) components. A good API makes it easier to develop a program by providing all the building blocks. A programmer then puts the blocks together.
+
+## A REST API
+Is a way for two computer systems to communicate using the HTTP technologies found in web browsers and servers.
+## 0-gather_data_from_an_API.py
+A Python script that, using this REST API https://jsonplaceholder.typicode.com/, for a given employee ID, returns information about his/her TODO list progress.
